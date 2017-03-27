f9e925cbf4eadedddd7a7672bb34872c7e1ff44f
John Didion <github@didion.net>
2017-03-27 15:48:06 -0400

    add script to generate example json

---
+++ multiqc/modules/atropos/example/run.sh
@@ -0,0 +1,1 @@
+atropos trim -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCACACAGTGATCTCGTATGCCGTCTTCTGCTTG -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT -pe1 tests/data/big.1.fq -pe2 tests/data/big.2.fq -o test1.fq -p test2.fq --report-file summary.json --stats both
